663c6fcd071e23b601a3871892a9bcf16c1c5a91
nnquy <nnquy2k3@gmail.com>
2025-09-12 14:27:50 +0700

    responsive for mobile

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Nav";
 import styles from "../styles/Home.module.css";
-import { FaGithub, FaInstagram, FaLink } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagram, FaLink } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { Link } from "react-scroll";
 
@@ -21,14 +21,14 @@ const Home: React.FC = () => (
     <p className={styles.title}>Fullstack Engineer</p>
     <Navbar />
     <div className={styles.socialLinks}>
-      <a href="https://www.facebook.com/nguyen.quy.682606/">
+      <a href="https://github.com/nguyenngocquy-23">
         <FaGithub />
       </a>
       <a href="https://www.linkedin.com/in/qu%C3%BD-nguy%E1%BB%85n-04543b2b3/">
         <FaLinkedin />
       </a>
-      <a href="">
-        <FaInstagram />
+      <a href="https://www.facebook.com/nguyen.quy.682606/">
+        <FaFacebook />
       </a>
       <a href="">
         <FaLink />
